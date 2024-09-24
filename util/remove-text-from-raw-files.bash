1d428e7f3a298cf2d609b21c72a83ec062148309
BCC <bcrounse@gmail.com>
2024-09-24 11:08:33 -0400

    started solcast

--- util/remove-text-from-raw-files.bash
+++ util/remove-text-from-raw-files.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Command line utility to replace specific text in a file
+# Command line utility to replace specific text in a set of files
 # Used to remove PID from raw solar files
+
+# Copy this to the util folder that is adjacent to the GMP folder that contains the raw data
 
 print_usage() {
   echo "Usage: remove-text-from-raw-files.bash old_text new_text"
@@ -48,9 +50,13 @@ new_text=$2
 # This is a version that should work with both BSD and GNU sed:
 # Per https://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux
 
-for file in ../data/*.csv; do
-    sed -i.bak "s/$old_text/$new_text/g" "$file"
+for file in ../GMP/*.csv; do
+    sed -i.bak "s|$old_text|$new_text|g" "$file"
 done
 
+mv ../GMP/*.csv.bak ../GMP/bak/ 
+
 # Remove the backup files we just made
-rm ../data/*.csv.bak
+# Do this MANUALLY once you've checked your work
+# rm ../GMP/bak/*.csv.bak
+
